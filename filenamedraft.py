61c0693e8e227fcd77e75ce332b99fcfca1ea68b
lzurek <lukasz.zurek91@gmail.com>
2019-02-19 21:17:37 +0100

    - added a visualization of files's for the user - added first draft of re_allNum() which will change all file names into one name ended with +1 number - added a sketch of a program's flow - added a test files folder directory with a few empty files for testing the program

--- filenamedraft.py
+++ filenamedraft.py
@@ -1,23 +1,49 @@
 """ 19-02-2019 - dev start
 
 Program ma na celu zautomatyzowanie zmiany nazwy wielu plików.
-Jeśli np. muszę dodać na końcu nazw kilku plików tekstowych ".red", to będę to mógł zrobić za pomocą tego programu
-Albo jeśli chcę, żeby x plików rozpoczynało się od "ŁŻ" - też zrobię to za pomocą programu.
+Jeśli np. muszę dodać na końcu nazw kilku plików tekstowych "_red", to będę to mógł zrobić za pomocą tego programu
+Albo jeśli chcę, żeby x plików rozpoczynało się od "ŁŻ_" - też zrobię to za pomocą programu.
 
 Na początku - niech zmienia nazwy plików, które zostały wrzucone do katalogu z plikiem programu.
 
 Co program będzie mógł robić?
-1) wyrzucać listę plików w katalogu
-2) umożliwiać dodawanie jakiegoś stringu do nazwy na początku lub na końcu
-*3) wspólną zmianę wybranych plików w katalogu, a nie wszystkich
+1) wyrzucać listę plików w katalogu - done
+2) zmieniać nazwę wszystkich plików w katalogu z rosnącą numeracją
+3) umożliwiać dodawanie jakiegoś stringu do nazwy na początku lub na końcu
+	- czyli zachowuję starą nazwę, ale dodaję coś do niej na końcu/na początku
+4) wspólną zmianę wybranych plików w katalogu, a nie wszystkich
+	- iterowanie poprzez range() (np. od pliku 1 do 10)
 
 """
 print("*** FILE RENAMER ***")
 
 import os, sys
 
+index = -1
+
 lista = os.listdir()
 
-printlist = input("Do you want to see the list of all files in the program's directory?[y/n]\n")
+
+def re_allNum(): #wersja dla zmiany nazw plików o tym samym rozszerzeniu
+	fileext = input("What is the MUTUAL file extension of the files that you want to rename? [e.g .txt] \n")
+	filename = input("Write a name that you want to give to all files in this directory: \n")
+	i = 0
+	for file in lista:
+		dst = filename + str(i) + fileext
+		os.rename(file, dst)
+		if file == "filenamedraft.py": #trying to prevent a program from changing its own filename
+			break
+			
+printlist = input("Do you want to see the list of all files in the program's directory? [y/n]\n")
 if printlist == "y":
-	print(lista)
+	for i in lista:
+		index += 1
+		print(str(index) + ") " + i)
+else:
+	break
+
+allfiles = input("Do you want to change ALL FILENAMES in the program's directory? [y/n]\n")
+if allfiles == "y":
+	#ścieżka programu ze zmianą wszystkich plików
+if allfiles == "n":
+	#ścieżka programu z wybraniem kilku plików
