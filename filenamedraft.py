69c7bffd1cdfc4392d7bdd2d0c58ccd5082a7346
lzurek <lukasz.zurek91@gmail.com>
2019-03-04 16:44:43 +0100

    removed some mistakes (PROGRAM'S DIRECTORY instead of correct form - PROGRAM'S FOLDER)

--- filenamedraft.py
+++ filenamedraft.py
@@ -4,7 +4,7 @@ import functions
 
 index = 0
 
-print("LIST OF FILES IN THE PROGRAM'S DIRECTORY (in short - PD):\n")
+print("LIST OF FILES IN THE PROGRAM'S FOLDER (in short - PF):\n")
 
 for i in functions.lista:
 	if i == "filenamedraft.py" or i == "__pycache__" or i == "functions.py":
@@ -12,7 +12,7 @@ for i in functions.lista:
 	index += 1
 	print(str(index) + ") " + i)
 
-allfilesoptions = input("Here's a list of functions that can change all files's names that you've copied into the PD:\n(1) Same name - different number (e.g. text1, text2, text3)\n(2) Add smtn at the begining/end of the filename (e.g. James_essay, James_notes, James_diary)\n")
+allfilesoptions = input("Here's a list of functions that can change all files's names that you've copied into the PF:\n(1) Same name - different number (e.g. text1, text2, text3)\n(2) Add smtn at the begining/end of the filename (e.g. James_essay, James_notes, James_diary)\n")
 if allfilesoptions == "1":
 	functions.re_allNum()
 if allfilesoptions == "2":
